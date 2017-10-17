699c0a7f6196c15f32f424af54d244becef50d7c
user <user>
2017-10-17 16:09:50 -0700

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UsersLibrary;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan lengthOfService = new DateTime(2017, 1, 20).Subtract(new DateTime(2009, 1, 20));
+            Employee Barack_Obama = new Employee("Obama II", "Barack", new DateTime(1961, 8, 4),
+                lengthOfService,"Президент Соединённых Штатов Америки");
+            Console.WriteLine(Barack_Obama);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ShapesLibrary;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ring ring = new Ring(new Point(5, 5), 20, 15);
+            Console.WriteLine(ring);
+
+            Console.ReadKey();
+        }
+    }
+}
